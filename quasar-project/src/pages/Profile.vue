3ef97fea6b30a4e064dd1a1b4cf5116f8e3055c4
Nurcan Kurt <nkurt430@gmail.com>
2023-12-30 17:12:25 +0300

    Update profile page

--- quasar-project/src/pages/Profile.vue
+++ quasar-project/src/pages/Profile.vue
@@ -1,29 +1,23 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="profile-container">
+  <q-page  class="row justify-center">
+    <div justify-content: center >
+      <div class="profile-container">
       <div class="avatar-container">
-          <q-img
-            src='https://picsum.photos/500/300'
-            ner-color="white"
-            style="height: 140px; max-width: 150px"
-          />
+        <q-avatar size = "200px">
+            <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+        </q-avatar>
       </div>
       <div class="info-container">
-        <q-input label="Username"
-                 filled
-                 v-model="username" />
-        <q-input label="Name"
-                 filled
-                 v-model="name" />
-        <q-input label="Bio"
-                 filled
-                 v-model="bio" />
+      <div>userName</div>
+      <div>follower</div>
+      <div>following</div>
       </div>
       <div class="action-container">
         <q-btn label="Save"
                color="primary"
                @click="saveProfile" />
       </div>
+    </div>
     </div>
   </q-page>
 </template>
@@ -39,19 +33,22 @@ export default defineComponent({
 <style scoped>
 .profile-container {
   width: 100%;
-  max-width: 600px;
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
 }
 
 .avatar-container {
   display: flex;
-  justify-content: center;
-  margin-bottom: 24px;
+  justify-content: left;
+  margin-bottom: 10px;
 }
 
 .info-container {
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   gap: 16px;
+  margin: 50px;
 }
 
 .action-container {
