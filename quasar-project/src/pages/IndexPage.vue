7804df43ab4c939868c75662cc1898d3d6c0f28d
Nurcan Kurt <nkurt430@gmail.com>
2023-12-30 11:08:02 +0300

    Add form

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,10 +1,49 @@
 <template>
   <q-page class="flex flex-center">
-    <img      
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 400px; height: 400px"
+    <!--
+Forked from:
+https://quasar.dev/vue-components/form#example--basic
+-->
+<div id="q-app" style="min-height: 100vh;">
+<div class="q-pa-md" style="max-width: 400px">
+
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
     >
+      <q-input
+        filled
+        v-model="name"
+        label="Your name *"
+        hint="Name and surname"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type something']"
+      ></q-input>
+
+      <q-input
+        filled
+        type="number"
+        v-model="age"
+        label="Your age *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'Please type your age',
+          val => val > 0 && val < 100 || 'Please type a real age'
+        ]"
+      ></q-input>
+
+      <q-toggle v-model="accept" label="I accept the license and terms"></q-toggle>
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"></q-btn>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm"></q-btn>
+      </div>
+    </q-form>
+
+  </div>
+</div>
+
   </q-page>
 </template>
 
@@ -15,4 +54,3 @@ export default defineComponent({
   name: 'IndexPage'
 })
 </script>
-
