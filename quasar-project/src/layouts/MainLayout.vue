864a548acf3860ab11f3fb6981d612b32b2df5e7
Ünal Ramazan DOĞANGÜN <c1928013@student.cankaya.edu.tr>
2023-12-30 13:12:05 +0300

    Update Left Menu

--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -12,7 +12,7 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Instagram
         </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
@@ -28,7 +28,7 @@
         <q-item-label
           header
         >
-          Essential Links
+          TestBees Team
         </q-item-label>
 
         <EssentialLink
@@ -51,46 +51,34 @@ import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
+    title: 'Home Page',
+    icon: 'home',
     link: 'https://quasar.dev'
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
+    title: 'Search',
+    icon: 'search',
     link: 'https://github.com/quasarframework'
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
+    title: 'Explore',
+    icon: 'explore',
     link: 'https://chat.quasar.dev'
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
+    title: 'Message',
+    icon: 'chat',
     link: 'https://forum.quasar.dev'
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    title: 'Create',
+    icon: 'add',
+    link: 'https://forum.quasar.dev'
   },
   {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+    title: 'Profile',
+    icon: 'person',
+    link: 'https://forum.quasar.dev'
   }
 ]
 
---
+++ quasar-project/src/pages/Profile.vue
@@ -0,0 +1,57 @@
+<template>
+    <q-page class="column flex-center text-white bg-black">
+       <q-card flat class="bg-black" style="width: 100%; max-width: 700px;">
+         <q-card-section class="text-center">
+           <div class="text-h4">{{ profile.name }}</div>
+           <div class="text-h6">{{ profile.title }}</div>
+         </q-card-section>
+   
+         <q-card-section>
+           <div class="text-h6">Followers</div>
+           <div class="text-h5">{{ profile.followers }}</div>
+           <div class="text-h6">Following</div>
+           <div class="text-h5">{{ profile.following }}</div>
+         </q-card-section>
+   
+         <q-card-section>
+           <div class="text-h6">Arşivi gör</div>
+           <div class="text-h5">{{ profile.archive }}</div>
+         </q-card-section>
+   
+         <q-card-section>
+           <div class="text-h6">ⓇETİKETLENENLER</div>
+           <div class="text-h5">{{ profile.tags }}</div>
+         </q-card-section>
+   
+         <q-card-section>
+           <div class="text-h6">Fotoğraflar Paylaş</div>
+           <div class="text-h5">{{ profile.images }}</div>
+         </q-card-section>
+   
+         <q-card-actions>
+           <q-btn color="white" text-color="black" label="İlk fotoğrafını paylaş" />
+         </q-card-actions>
+       </q-card>
+    </q-page>
+   </template>
+   
+   <script>
+   export default {
+    data() {
+       return {
+         profile: {
+           name: "Your Name",
+           title: "Your Title",
+           followers: 17,
+           following: 8,
+           archive: "archive details",
+           tags: "tags details",
+           images: "images details",
+         },
+       };
+    },
+   };
+   </script>
+   
+   <style>
+   </style>
